7f6ce65a9239e934244d4a243c3bbaf2774437cd
SAKHI Abdelkader <abdelkader.sakhi.pro@gmail.com>
2020-07-10 15:04:56 +0200

    update in app style and index

---
+++ app.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoader', () => {
+    const squares = document.querySelectorAll('.grid div');
+    const result = document.querySelector('#result');
+    const displayCurrentPlayer = document.querySelector('#current-player');
+
+
+
+    
+});
+
+
+
+
+
+
+
+
+
+
+
